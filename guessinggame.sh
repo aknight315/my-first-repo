0106a44e9b077f9af9869b2567e0ced839dec913
aknight315 <amber.dielle.knight@gmail.com>
2019-12-04 21:34:41 -0500

    final reedited script

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,34 @@
-#!/usr/bin/env bash
-#Guessing game created for unixbash final assignment for coursera!
-echo "[Welcome]"
-function ask {
-      echo "How many files are in the current directory:"  
+#!/bin/bash
+
+##Created for final assignment for coursera##
+echo "Hello!!"
+function ask { 
+echo "How many files are in the curent directory?"
 read guess
-files=$(ls -l | wc -l)
 }
+files=$(ls -q | wc -l)
+##This is a value test##
+##echo "$files"
 ask
-while [[$guess -ne $files]]
+
+##This is asking while the guess is not equal to file number do## 
+while (( "$guess" != "$files" ))
 do
-       if [[$guess -lt $files]]
-       then
-   echo "Too high, try again"
-   else
-     echo "Too low, try again"
-    fi 
-  ask
-Done
+##While reading guess if too low say "Too low try again"##
+if (( "$guess" > "$files" )); 
+	then
+	echo "Too high try again"
+ask
+##While reading guess if too high say "Too high try again##
+elif (( "$guess" < "$files" ));
+	then
+	echo "Too low try again"
+ask
+elif (( "$guess" == "$files" ));
+        then
+        echo "Congrats"
+##If correct at this point say Congrats and finish the do with done##
+fi
+done
+
 echo "Congrats"
